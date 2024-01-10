b0dd4891b815c5b88bcde8d650446d0c8c23cbfb
yanchim <yanchi.x@foxmail.com>
2024-01-10 22:40:03 +0800

    Add dir-locals for cider

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((cider-default-cljs-repl . shadow)
+         (cider-shadow-default-options . "app"))))
